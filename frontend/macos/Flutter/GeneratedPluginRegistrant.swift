5ae2874a78a50c4d16d41bc02a6eb7fc56641b32
Nitson_Lopez <lopezyustesnitson13@gmail.com>
2026-08-24 19:40:46 -0500

    Descripción de los cambios realizados

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
-=======
 import file_selector_macos
->>>>>>> origin/feature/diego
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-<<<<<<< HEAD
-=======
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
->>>>>>> origin/feature/diego
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
